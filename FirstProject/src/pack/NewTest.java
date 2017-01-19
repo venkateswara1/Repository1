db4892a23ef80bada35841b0e18d116beb3d68b3
githubforgmi <gudugunuri92@gmail.com>
2017-01-19 12:42:52 +0530

    firstcomit

---
+++ FirstProject/src/pack/NewTest.java
@@ -0,0 +1,33 @@
+package pack;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.annotations.AfterMethod;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("kjhdsf");
+	  
+	  List<String> l1=new ArrayList<String>();
+	  l1.add("aaa");
+	  l1.add("bbb");
+	  l1.add("ccc");
+	  List<String> l2=new ArrayList<String>();
+	  l2.add("aaa");
+	  System.out.println(l1.contains(l2.get(0)));
+  }
+  
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+}
